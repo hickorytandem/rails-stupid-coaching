77f26e170a137ca8342418d8bd2e57766d5dba9b
Michael Carter <hickorytandem@gmail.com>
2021-06-29 21:51:14 +0900

    finished, but need to work on design

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.downcase == "i am going to work right now!"
+      @coach_answer = "Great!"
+    elsif @question.end_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
